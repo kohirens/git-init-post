74bdd06fb36e119165741de0dfebfb0d135750e7
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-02-19 20:23:40 -0500

    Fixed typo in comment.

--- error.go
+++ error.go
@@ -4,6 +4,6 @@ var errors = struct {
 	invalidCommitRange,
 	subCmdMissing string
 }{
-	invalidCommitRange: "invalid commit range format. please use for example `commit1..commit2`. Exceptable values are a branch, tag, or hash.",
+	invalidCommitRange: "invalid commit range format. please use for example `commit1..commit2`. Acceptable values are a branch, tag, or hash.",
 	subCmdMissing:      "missing a sub command, either \"version\" or \"taggable\"",
 }
