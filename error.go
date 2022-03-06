78c1e4af39adce5e68812d602d552539792bb652
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-03-06 13:38:14 -0500

    Update Command Line Arguments
    chg: Updated CLI arg instructions.
    add: Version flag.
    fix: Message types.
    Refactored semver sub-command.

--- error.go
+++ error.go
@@ -2,8 +2,12 @@ package main
 
 var errors = struct {
 	invalidCommitRange,
-	subCmdMissing string
+	subCmdMissing,
+	packageNameRequired,
+	semverFormtatInvalid string
 }{
-	invalidCommitRange: "invalid commit range format. please use for example `commit1..commit2`. Acceptable values are a branch, tag, or hash.",
-	subCmdMissing:      "missing a sub command, either \"semver\" or \"taggable\"",
+	invalidCommitRange:   "invalid commit range format. please use for example `commit1..commit2`. Acceptable values are a branch, tag, or hash.",
+	subCmdMissing:        "missing a sub command, either \"semver\" or \"taggable\"",
+	packageNameRequired:  "packageName flag is required when --format equals \"go\"",
+	semverFormtatInvalid: "invalid format set, options are \"go\" or \"JSON\"",
 }
